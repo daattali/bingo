f90f93b1877097dc5d00c0ec9bc96afa3f6c0216
Dean Attali <daattali@gmail.com>
2016-06-13 09:23:14 -0700

    Create lala.R

---
+++ inst/topics/lala.R
@@ -0,0 +1,6 @@
+c(
+"kkkkkkk","kjkj","mmmmmm","kjhkj","huh","kmbnmbn","yug","ygyghjhlkjjj",
+"hkjhj",
+"kjhgkj",
+"hjkjn","jcvnj","kjhyuhuj","nmbnmjbn","lkjhk","lkjhuj","kjhgiuhkj","kjhiukj","kjhuk","mf","dghkj","iuythu"
+)
