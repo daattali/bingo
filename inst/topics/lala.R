faa46ecddce17dd3a617b7855feadc220ea4e514
Dean Attali <daattali@gmail.com>
2016-06-13 09:25:25 -0700

    Update lala.R

--- inst/topics/lala.R
+++ inst/topics/lala.R
@@ -1,5 +1,5 @@
 c(
-"kkkkkkk","kjkj","mmmmmm","kjhkj","huh","kmbnmbn","yug","ygyghjhlkjjj",
+"kkkkkkk","kjkj","mmmmmm","kjhkj","huh","kmbnmbn","yug","ygyghjhlkjjj"
 "hkjhj",
 "kjhgkj",
 "hjkjn","jcvnj","kjhyuhuj","nmbnmjbn","lkjhk","lkjhuj","kjhgiuhkj","kjhiukj","kjhuk","mf","dghkj","iuythu"
